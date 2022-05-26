f48c0f572e1c3383a82ffd0036bd8fa0fdecadc6
igalmes <ignaciogalmes79@gmail.com>
2022-05-26 12:48:51 -0300

    git push

--- js/funciones.js
+++ js/funciones.js
@@ -1,18 +1,28 @@
-setTimeout(function() {
-    alert('Pasaron unos minutos desde que abriste mi CV. ¡Espero que hayas podido hecharle un vistazo!');
+// setTimeout(function() {
+//     alert('Pasaron unos minutos desde que abriste mi CV. ¡Espero que hayas podido hecharle un vistazo!');
 
-}, 120000);
+// }, 120000);
 
-const toggle = (element) => {
-    let items = document.getElementsByClassName('item');
-    let a = element.querySelectorAll('a');
-    for (let i = 0; i < items.length; i++) {
-        if (items[i].classList.contains('active')){
-            items[i].classList.remove('active');
-            a[0].innerHTML = `<i class='fas fa-bars'></i>`;
-        }else{
-            items[i].classList.add('active');
-            a[0].innerHTML = `<i class='fas fa-times'></i>`;
-        }         
+
+function menu () {
+    var x = document.getElementById("mylinks");
+    if (x.style.display === "block") {
+    x.style.display = "none";
+    } else {x.style.display = "block";
     }
 }
+
+
+// const toggle = (element) => {
+//     let items = document.getElementsByClassName('item');
+//     let a = element.querySelectorAll('a');
+//     for (let i = 0; i < items.length; i++) {
+//         if (items[i].classList.contains('active')){
+//             items[i].classList.remove('active');
+//             a[0].innerHTML = `<i class='fas fa-bars'></i>`;
+//         }else{
+//             items[i].classList.add('active');
+//             a[0].innerHTML = `<i class='fas fa-times'></i>`;
+//         }         
+//     }
+// }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-function miFuncion() {
-    swal({
-        title: 'Hola me encanta la comida y viajar',
-        timer: 13000
-    });
+// function miFuncion() {
+//     swal({
+//         title: 'Hola me encanta la comida y viajar',
+//         timer: 3000
+//     });
 
+// }
+function saludo (nombre){
+    swal(`Hola ${nombre} gracias por visitarnos, no olvides likear mi página`);
 }
+
+
+function saludoUsuario(cb) {
+    var nombre = prompt('Ingresa tu nombre');
+    cb(nombre);
+    
+}
+
+saludoUsuario(saludo);
